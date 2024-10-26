90ae93bfcb2771119874f396db17a3dea1824a5b
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-10-26 19:56:11 +0500

    Update page.tsx

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,18 +1,15 @@
 "use client"
 
 import React from 'react';
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Contact() {
-
   const notify = () => toast("submit....");
 
-  async function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
+  async function handleSubmit(event: any) {
     event.preventDefault();
     const formData = new FormData(event.target);
-
     formData.append("access_key", "dbda848b-a2a9-4954-88c7-5e5362feae49");
 
     const object = Object.fromEntries(formData);
@@ -34,8 +31,7 @@ export default function Contact() {
 
   return (
     <>
-
-      <section data-aos="fade-down" data-aos-duration="3000">
+      <section data-aos="fade-down" data-aos-duration="3000" className="bg-white">
         <div
           id="map"
           className="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat"
@@ -49,51 +45,44 @@ export default function Contact() {
           />
         </div>
         <div className="container px-6 md:px-12">
-          <div className="block rounded-lg bg-slate-700 px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px] border border-gray-300">
+          <div className="block rounded-lg bg-slate-700 px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px] border border-gray-300">
             <div className="flex flex-wrap">
               <div className="mb-13 w-full shrink-0 grow-0 basis-auto md:px-3 lg:mb-0 lg:w-5/12 lg:px-6">
                 <form onSubmit={handleSubmit}>
-                  <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <div className="relative mb-6">
                     <input
                       type="text"
-                      className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
+                      className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary"
                       id="example" name="name" required placeholder="Name"
                     />
                     <label
-                      className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                      className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary"
                       htmlFor=""
-                    >
-                    </label>
+                    ></label>
                   </div>
-                  <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <div className="relative mb-6">
                     <input
                       type="email"
-                      className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
-                      id="exampleInput91" name="email" required placeholder=" Email address"
+                      className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary"
+                      id="exampleInput91" name="email" required placeholder="Email address"
                     />
                     <label
-                      className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
+                      className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary"
                       htmlFor="email"
-                    >
-
-                    </label>
+                    ></label>
                   </div>
-                  <div className="relative mb-6" data-te-input-wrapper-init="">
+                  <div className="relative mb-6">
                     <textarea
-                      className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
+                      className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100"
                       id="exampleFormControlTextarea1"
                       rows={3}
                       defaultValue={""} required placeholder="Message"
                     />
                     <label
                       htmlFor="message"
-                      className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                    >
-
-                    </label>
+                      className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary"
+                    ></label>
                   </div>
-                 
-                </div>  */}
                   <button
                     onClick={notify}
                     type="submit"
@@ -101,17 +90,16 @@ export default function Contact() {
                   >
                     Submit
                   </button>
-
-
                   <ToastContainer />
                 </form>
               </div>
               <div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
                 <div className="flex flex-wrap">
+                
                   <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
                     <div className="flex items-start">
                       <div className="shrink-0">
-                        <div className="inline-block rounded-md bg-sky-200 p-4 text-primary">
+                        <div className="inline-block rounded-md bg-sky-300 p-4 text-primary">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -123,28 +111,15 @@ export default function Contact() {
                             <path
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                              d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z"
+                              d="M9 12h6m-3-3v6"
                             />
                           </svg>
                         </div>
                       </div>
+                      
                       <div className="ml-6 grow">
-                        <p className="mb-2 font-bold ">Technical support</p>
+                        <p className="mb-2 font-bold">GMAIL</p>
                         <p className="text-sm text-white">memonbisma22@gmail.com</p>
-                      </div>
-                    </div>
-                  </div>
-                
-                      <div className="ml-6 grow">
-                        <p className="mb-2 font-bold ">Address</p>
-                        <p className="text-sm text-white">
-                          Karachi, Pakistan.<br />
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                           
-                        </div>
                       </div>
                     </div>
                   </div>
@@ -154,8 +129,6 @@ export default function Contact() {
           </div>
         </div>
       </section>
-
-
     </>
   );
 }
